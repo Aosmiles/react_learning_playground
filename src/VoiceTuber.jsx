327f087136ad680d503df67555305067590e8317
Aosmiles <arlin.smiles@gmail.com>
2022-10-06 16:30:16 -0700

    move code to new voice tuber component

---
+++ src/VoiceTuber.jsx
@@ -0,0 +1,52 @@
+﻿import { useRef, useState } from "react";
+import ImageUploader from "./ImageUploader.jsx";
+
+function VoiceTuber() {
+  const [talkImage, setTalkImage] = useState("");
+  const [idleImage, setIdleImage] = useState("");
+  const [currentImage, setCurrentImage] = useState("");
+
+  const audioCtx = useRef(null);
+  const analyzer = useRef(null);
+
+  async function getAudio() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    audioCtx.current = new AudioContext();
+    analyzer.current = audioCtx.current.createAnalyser();
+    const source = audioCtx.current.createMediaStreamSource(stream);
+    source.connect(analyzer.current);
+    // How much data should we collect
+    analyzer.fftSize = 2 ** 10;
+    // pull the data off the audio
+
+    drawTimeData();
+  }
+
+  function drawTimeData() {
+    const array = new Uint8Array(analyzer.current.frequencyBinCount);
+    analyzer.current.getByteFrequencyData(array);
+    const arraySum = array.reduce((a, value) => a + value, 0);
+    const average = Math.round(arraySum / array.length);
+    if (average > 6) {
+      console.log(average);
+      setCurrentImage(talkImage);
+    } else {
+      setCurrentImage(idleImage);
+    }
+
+    // call itself as soon as possible
+    requestAnimationFrame(() => drawTimeData());
+  }
+
+  return (
+    <div>
+      <h1>hello</h1>
+      <button onClick={getAudio}>mic</button>
+      <ImageUploader image={talkImage} onImageChange={setTalkImage} />
+      <ImageUploader image={idleImage} onImageChange={setIdleImage} />
+      <img src={currentImage} width={200} alt="" className="pixel-image" />
+    </div>
+  );
+}
+
+export default VoiceTuber;
