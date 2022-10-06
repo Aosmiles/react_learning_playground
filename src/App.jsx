fc9c6c57c448432f61f6daae27deaea38f216ea4
Aosmiles <arlin.smiles@gmail.com>
2022-10-06 11:21:06 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+﻿import ImageUploader from "./ImageUploader.jsx";
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [talkImage, setTalkImage] = useState("");
+  const [idleImage, setIdleImage] = useState("");
+  const [isMicOn, setIsMicOn] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+
+  const audioCtx = useRef(null);
+  const analyzer = useRef(null);
+
+  async function getAudio() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    setIsMicOn(true);
+    audioCtx.current = new AudioContext();
+    analyzer.current = audioCtx.current.createAnalyser();
+    const source = audioCtx.current.createMediaStreamSource(stream);
+    source.connect(analyzer.current);
+    // How much data should we collect
+    analyzer.fftSize = 2 ** 10;
+    // pull the data off the audio
+
+    drawTimeData();
+  }
+
+  function drawTimeData() {
+    const array = new Uint8Array(analyzer.current.frequencyBinCount);
+    analyzer.current.getByteFrequencyData(array);
+    const arraySum = array.reduce((a, value) => a + value, 0);
+    const average = Math.round(arraySum / array.length);
+    if (average > 6) {
+      console.log(average);
+      setCurrentImage(talkImage);
+    } else {
+      setCurrentImage(idleImage);
+    }
+
+    // call itself as soon as possible
+    requestAnimationFrame(() => drawTimeData());
+  }
+
+  return (
+    <div>
+      <h1>hello</h1>
+      <button onClick={getAudio}>mic</button>
+      <ImageUploader image={talkImage} onImageChange={setTalkImage} />
+      <ImageUploader image={idleImage} onImageChange={setIdleImage} />
+      <img src={currentImage} width={200} alt="" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ImageUploader.jsx
@@ -0,0 +1,28 @@
+﻿function ImageUploader({ image, onImageChange }) {
+  function handleFile(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => onImageChange(e.target.result);
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
+
+  return (
+    <div>
+      <label>
+        upload:
+        <input
+          type="file"
+          accept=".jpg, .jpeg, .png, .gif"
+          onChange={handleFile}
+        />
+      </label>
+      <img src={image} width={200} alt="" />
+    </div>
+  );
+}
+
+export default ImageUploader;
